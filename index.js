2e48e3b6f7f4e09981d455a83fdbd5d6cb0c7a7d
Jean Marcel <jeanmarcel.rj@gmail.com>
2022-05-21 03:28:46 -0300

    Exercício Fizz Buzz concluído e documentado!

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 function FizzBuzz(numero){
+
+    /* Se, o resto da divisão do numero inserido for igual a '0', este, é divisível por '3' e '5',
+    e retornará o valor FizzBuzz!  */
+
     if(numero % 3 === 0 && numero % 5 === 0){
         return console.log('FizzBuzz')
     } 
+
+    /* Se, o resto da divisão do numero inserido for igual a '0', este, é divisível por '3',
+    e retornará a string 'Fizz' */
+
     else if(numero % 3 === 0){
         return console.log('Fizz')
     }
+
+    /* Se, o resto da divisão do numero inserido for igual a '0', este, é divisível por '5'
+    e retornará a string 'Buzz' */
+
     else if(numero % 5 === 0){
         return console.log('Buzz')
     } 
-    
 }
 
-FizzBuzz(45)
+FizzBuzz();  // Inserir o número que será analisado //
